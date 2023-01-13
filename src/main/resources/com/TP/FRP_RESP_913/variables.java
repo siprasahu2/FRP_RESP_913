4ff054a658b131a9b62b007323ec6404542d63e6
Sipra Sahu <ssah0002@Synchronoss.com>
2023-01-13 12:09:26 +0530

    res

---
+++ src/main/resources/com/TP/FRP_RESP_913/variables.java
@@ -0,0 +1,19 @@
+package com.TP.FRP_RESP_913;
+
+public class variables{
+public ilog.rules.factory.IlrReflect reflect;
+public ilog.rules.xml.IlrXmlObject object;
+public ilog.rules.factory.IlrReflect getReflect() {
+return reflect;
+}
+public void setReflect(ilog.rules.factory.IlrReflect reflect){
+this.reflect=reflect;
+}
+public ilog.rules.xml.IlrXmlObject getObject() {
+return object;
+}
+public void setObject(ilog.rules.xml.IlrXmlObject object){
+this.object=object;
+}
+
+}
